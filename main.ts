b911449097730cab8a17be11c70d2cc05f6c0b52
apple471296 <71689201+apple471296@users.noreply.github.com>
2020-11-24 15:35:50 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+let a = 0
+let b = 0
+input.onButtonPressed(Button.AB, function () {
+    a = randint(0, 6)
+    basic.showNumber(a)
+    basic.pause(100)
+    b = randint(0, 6)
+    basic.showNumber(b)
+    basic.pause(100)
+    basic.clearScreen()
+    if (a < b) {
+        basic.showString("B win")
+    } else if (b < a) {
+        basic.showString("A win")
+    }
+    basic.clearScreen()
+})
