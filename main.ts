727cef03b8b2b5cc0e7030af6d8dbdd9c9eda171
apple471296 <71689201+apple471296@users.noreply.github.com>
2020-11-24 15:39:25 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,11 +3,12 @@ let b = 0
 input.onButtonPressed(Button.AB, function () {
     a = randint(0, 6)
     basic.showNumber(a)
-    basic.pause(100)
+    basic.pause(200)
     basic.clearScreen()
+    basic.pause(200)
     b = randint(0, 6)
     basic.showNumber(b)
-    basic.pause(100)
+    basic.pause(200)
     basic.clearScreen()
     if (a < b) {
         basic.showString("B win")
